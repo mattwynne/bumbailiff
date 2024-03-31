a25e8d585e36631e50b34b46361a94e6b71ec607
Matt Wynne <matt@mattwynne.net>
2024-03-31 00:03:44 -0700

    WIP

--- tests/no-todos.sh
+++ tests/no-todos.sh
@@ -1,5 +1,9 @@
+export GIT_AUTHOR_NAME="Daisy Developer"
+export GIT_AUTHOR_EMAIL="daisy@developer.com"
 mkdir -p tmp/git/no-todos
 cd tmp/git/no-todos
 git init --quiet .
+git config commit.gpgSign false
+git commit --quiet -m "Initial commit" --allow-empty
 bumbailiff
 echo $?
