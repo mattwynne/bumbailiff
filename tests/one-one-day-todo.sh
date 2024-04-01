bd59f49974c4b4651be6cb7671f3c03a05270442
Matt Wynne <matt@mattwynne.net>
2024-03-31 20:01:04 -0700

    Refactoring complete!

--- tests/one-one-day-todo.sh
+++ tests/one-one-day-todo.sh
@@ -1,7 +1,8 @@
-yesterday=$(date -v -1d) # OSX/BSD only!
+yesterday=$(date -v -25H) # OSX/BSD only!
 export GIT_AUTHOR_NAME="Daisy Developer"
 export GIT_AUTHOR_EMAIL="daisy@developer.com"
 export GIT_AUTHOR_DATE="$yesterday"
+export GIT_COMMITTER_DATE="$yesterday"
 mkdir -p tmp/git/one-zero-day-todo
 cd tmp/git/one-zero-day-todo
 git init --quiet .
